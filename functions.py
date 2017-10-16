85e2fc9f831e685967a5ba0babb6e26e85e51d7a
lovingzw <wangguizheng1995@hotmail.com>
2017-10-16 10:00:19 -0500

    Add files via upload

--- functions.py
+++ functions.py
@@ -28,14 +28,14 @@ def decision(d, trd, a, b):
     return index
 
 
-def operation(data, detail_last_min, info, operate_index, transaction, trd, underwear,):
+def operation(data, detail_last_min, info, operate_index, transaction, trd, underwear):
     p = detail_last_min[0]
     avg_price = np.mean(data[operate_index, 0:3])
     lot_value = avg_price * info.unit_per_lot[operate_index] * info.margin_rate[operate_index]
 
     money_pool = detail_last_min[1] - underwear
     volume = np.round(money_pool / (lot_value * (1. + transaction)))
-    if detail_last_min[1] > underwear:
+    if detail_last_min[4] > underwear:
         if trd == 1:
             p[operate_index] = volume
         else:
@@ -45,11 +45,19 @@ def operation(data, detail_last_min, info, operate_index, transaction, trd, unde
 
 def adjust(detail_last_min, memory, timer):
     p = detail_last_min[0]
-    if detail_last_min[2] - memory.asset < -50000:  # stop loss
+    if (detail_last_min[4] - memory.top) / memory.top < -0.1:  # stop loss
         memory.status = -1
         memory.stop_loss_time = timer
         p = np.zeros(13, dtype=np.int)
+        print("stop loss")
     return p
+
+
+def peak_update(detail_last_min, memory):
+    peak = memory.top
+    if detail_last_min[4] > memory.top:
+        peak = detail_last_min[4]
+    return peak
 
 
 def close(memory, timer, data):
@@ -57,12 +65,9 @@ def close(memory, timer, data):
     duration = timer - memory.operate_time
     s1 = (data[memory.operate_index, 3] - memory.operate_price) / (memory.operate_price * duration)
     s2 = (data[memory.target_index, 3] - memory.target_price) / (memory.target_price * duration)
-    if (s1 >= 2 * s2 and memory.trend == 1) or (s1 <= 2 * s2 and memory.trend == -1):
+    if (s1 >= 1.5 * s2 and memory.trend == 1) or (s1 <= 1.5 * s2 and memory.trend == -1):
         c = True
     return c
-
-
-
 
 
 def buy(position, index, amount_cash, data, info, transaction):
--- strategy.py
+++ strategy.py
@@ -1,5 +1,5 @@
 import numpy as np
-from functions import trend, gradient, decision, operation, close, adjust
+from functions import trend, gradient, decision, operation, close, adjust, peak_update
 
 asset_index1 = 7  # define item 1 as coke
 asset_index2 = 8  # define item 2 as coal
@@ -7,24 +7,21 @@ underwear = 9000000  # define balance limit
 
 
 def handle_bar(timer, data, info, init_cash, transaction, detail_last_min, memory):
+    if timer == 0:
+        memory.status = 0  # -1, stop loss; 0, ready; 1, open
 
     position_new = detail_last_min[0]
 
-    if timer == 0:
-        memory.status = 0  # -1, stop loss; 0, ready; 1, open
-        memory.asset = init_cash
-
-    if  memory.status == -1:
+    if memory.status == -1:
         if (timer - memory.stop_loss_time) % 120 == 0:
             memory.status = 0
-
-    if memory.status == 0:  # ready to open
+    elif memory.status == 0:  # ready to open
 
         gradient1 = gradient(asset_index1, data)
         gradient2 = gradient(asset_index2, data)
         delta = gradient1 - gradient2
 
-        if abs(delta) > 3 * transaction:  # just to ensure interest
+        if abs(delta) > 2 * transaction:  # just to ensure interest
 
             current_trend = trend(asset_index1, asset_index2, data)  # trend detection
             index = decision(delta, current_trend, asset_index1, asset_index2)  # item decision
@@ -37,18 +34,17 @@ def handle_bar(timer, data, info, init_cash, transaction, detail_last_min, memor
             memory.operate_time = timer
             memory.trend = current_trend
 
-            position_new = operation(data, detail_last_min, info, operate_index, transaction, current_trend, underwear,)
+            position_new = operation(data, detail_last_min, info, operate_index, transaction, current_trend, underwear)
             memory.position = position_new
-            memory.asset = detail_last_min[2]
+            memory.top = detail_last_min[4] - underwear
             memory.status = 1
-
     else:
-        if memory.status == 1:  # adjustment (stop loss or close out)
-            position_new = adjust(detail_last_min, memory, timer)
+        memory.top = peak_update(detail_last_min, memory)
+        position_new = adjust(detail_last_min, memory, timer)
 
-            if close(memory, timer, data):  # close out
-                memory.status = 0
-                position_new = np.zeros(13, dtype=np.int)
+        if close(memory, timer, data):  # close out
+            memory.status = 0
+            position_new = np.zeros(13, dtype=np.int)
 
     return position_new, memory
 
