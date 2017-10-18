0827d42f788cd22f6c10019549bba4289e53e13b
lovingzw <wangguizheng1995@hotmail.com>
2017-10-19 02:26:47 +0800

    Add files via upload

--- strategy.py
+++ strategy.py
@@ -1,11 +1,9 @@
 import numpy as np
-from functions import pair_open
 
-index1 = 7  # define item 1 as coke
-index2 = 8  # define item 2 as coal
+index1 = 7  # define item as coke
 underwear = 5000000  # define balance limit
-threshold_open = 1
-threshold_close = 0.1
+vol_threshold = 1.8
+partition = 0.9
 
 
 def handle_bar(timer, data, info, init_cash, transaction, detail_last_min, memory):
@@ -14,46 +12,54 @@ def handle_bar(timer, data, info, init_cash, transaction, detail_last_min, memor
     if timer == 0:
         memory.status = 0  # -1, stop loss; 0, ready; 1, open
         memory.s1 = np.mean(data[index1, 0:3])
-        memory.s2 = np.mean(data[index2, 0:3])
+        memory.vol = data[index1, 4]
     elif timer == 1:
         memory.s1 = np.array([memory.s1, np.mean(data[index1, 0:3])])
-        memory.s2 = np.array([memory.s2, np.mean(data[index2, 0:3])])
+        memory.vol = np.array([memory.vol, data[index1, 4]])
     elif 0 < timer < 60:
         memory.s1 = np.append(memory.s1, np.mean(data[index1, 0:3]))
-        memory.s2 = np.append(memory.s2, np.mean(data[index2, 0:3]))
+        memory.vol = np.append(memory.vol, data[index1, 4])
     else:
-        memory.s1 = np.append(memory.s1[1:59], np.mean(data[index1, 0:3]))
-        memory.s2 = np.append(memory.s2[1:59], np.mean(data[index2, 0:3]))
-        diff = memory.s1 - memory.s2
-        delta = np.mean(data[index1, 0:3]) - np.mean(data[index2, 0:3])
-        avg5 = np.mean(diff[55:59])
-        avg10 = np.mean(diff[50:59])
-        avg60 = np.mean(diff)
-        std60 = np.std(diff)
-        zscore = (delta - avg60) / std60
+        current_price = np.mean(data[index1, 0:3])
+        current_vol = data[index1, 4]
+        memory.s1 = np.append(memory.s1[1:59], current_price)
+        memory.vol = np.append(memory.s1[1:59], current_vol)
 
-        if memory.status == -1:
+        avg10_price = np.mean(memory.s1[-10:-1])
+        #avg10_vol = np.mean(memory.vol[-10:-1])
+        #avg60_price = np.mean(memory.s1)
+        avg60_vol = np.mean(memory.vol)
+        if current_vol > avg60_vol * vol_threshold:
+            if current_price > avg10_price:
+                trend = 1
+            else:
+                trend = -1
+        else:
+            trend = 0
+
+        if memory.status == -1:  # hold after stop loss
             if (timer - memory.stop_loss_time) % 120 == 0:
                 memory.status = 0
         elif memory.status == 0:  # ready to open
-            if abs(zscore) > threshold_open:
-                money_pool = detail_last_min[4] - underwear
-                position_new = pair_open(detail_last_min, data, info, transaction, index1, index2, money_pool, zscore)
+            if trend != 0 and detail_last_min[4] > underwear:
+                money_pool = (detail_last_min[1] - underwear) * partition
+                lot_value = current_price * info.unit_per_lot[index1] * info.margin_rate[index1]
+                position_new[index1] = trend * np.floor(money_pool / (lot_value * (1. + transaction)))
                 memory.top = detail_last_min[4]
                 memory.total = detail_last_min[4]
                 memory.status = 1
+                memory.trend = trend
         else:
             memory.top = np.maximum(detail_last_min[4], memory.top)
             if (detail_last_min[4] - memory.top) / memory.top < -0.08:  # stop loss
                 memory.stop_loss_time = timer
                 position_new[index1] = 0
-                position_new[index2] = 0
                 memory.status = -1
                 print('stop loss')
-            elif abs(zscore) < threshold_close and detail_last_min[4] > memory.total * 1.0001:  # close out
-                position_new[index1] = 0
-                position_new[index2] = 0
-                memory.status = 0
+            else:
+                if trend == 0:  # close out
+                    position_new[index1] = 0
+                    memory.status = 0
 
     return position_new, memory
 
